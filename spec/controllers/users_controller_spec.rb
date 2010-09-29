dec1c1220c2ef16afa8b89a3d005bdac73dbf2d9
Mikael Amborn <ma@mima.x.se>
2010-09-29 20:48:31 +0200

    added edit page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -69,6 +69,25 @@ describe UsersController do
     
   end
   
+  describe "GET 'edit'" do
+    before(:each) do
+      @user = Factory(:user)
+      test_sign_in(@user)
+    end
+    
+    it "should be successful" do
+      get :edit, :id => @user
+      response.should have_selector("title", :content => "Edit user")
+    end
+    
+    it "should have a link to change the Gravatar" do
+      get :edit, :id => @user
+      gravatar_url = "http://gravatar.com/emails"
+      response.should have_selector("a", :href => gravatar_url,
+                                         :content => "change")
+    end
+  end
+  
   describe "POST 'create'" do
      
     describe "failure" do
