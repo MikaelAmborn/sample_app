393b4a00d9f0f3380268f9d45dec627c3b5c7464
Mikael Amborn <ma@mima.x.se>
2010-09-30 21:53:46 +0200

    added tests for edit page

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -20,7 +20,11 @@ module SessionsHelper
   def sign_out
     cookies.delete(:remember_token)    
     self.current_user = nil
-  end   
+  end 
+  
+  def deny_access
+    redirect_to signin_path, :notice => "Please sign in to access this page."
+  end  
    
   private 
     def user_from_remember_token
